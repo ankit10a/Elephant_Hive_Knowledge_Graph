dad4aaccfa3008376c47dfc07f9c81a8e0cb87f5
SaurabhSharma2388 <128196839+SaurabhSharma2388@users.noreply.github.com>
2023-07-03 09:13:31 +0000

    New Update

--- main.py
+++ main.py
@@ -5,31 +5,29 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+  if request.method == 'GET':
+    return render_template('index.html')
+  elif request.method == 'POST':
+    url = request.form['url']
+    openai.api_key = request.form['api_key']
+    response = openai.Completion.create(
+      engine='text-davinci-003',
+      prompt=f"Generate knowledge graph for {url}.",
+      max_tokens=200,
+      temperature=0.5,
+      n=1,
+      stop=None,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0)
 
+    # Extract the graph from the API response
+    graph = response.choices[0].text.strip()
 
-@app.route('/', methods=['POST'])
-def process_url():
-  url = request.form['url']
-  openai.api_key = request.form['api_key']
-  response = openai.Completion.create(
-    engine='text-davinci-003',
-    prompt=f"Generate knowledge graph for {url}.",
-    max_tokens=200,
-    temperature=0.5,
-    n=1,
-    stop=None,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0)
-
-  # Extract the graph from the API response
-  graph = response.choices[0].text.strip()
-
-  # Return the graph to be displayed in the graph container
-  return graph
+    # Return the graph to be displayed in the graph container
+    return graph
 
 
 if __name__ == '__main__':
