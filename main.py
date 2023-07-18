003d2e4dba6bb53a82ced029c6495b2d730d73d3
SaurabhSharma2388 <128196839+SaurabhSharma2388@users.noreply.github.com>
2023-07-18 15:20:15 +0000

    update

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request
 import openai
 import networkx as nx
+import matplotlib
+
+matplotlib.use('Agg')  # Use the Agg backend
+
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+# Set the maximum number of nodes and edges to limit the graph complexity
+MAX_NODES = 100
+MAX_EDGES = 200
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -35,6 +43,9 @@ def index():
 
       graph_data = convertGraphToTree(graph)
 
+      # Limit the number of nodes and edges in the graph
+      graph_data = limitGraphComplexity(graph_data, MAX_NODES, MAX_EDGES)
+
       # Generate the graph image
       fig, ax = plt.subplots(figsize=(20, 6), dpi=72)
       pos = nx.spring_layout(graph_data)
@@ -62,11 +73,23 @@ def convertGraphToTree(graph):
     if "->" in item:
       source, target = item.split("->")
       G.add_edge(source.strip(), target.strip())
-      pos = nx.shell_layout(G)  # or nx.circular_layout(G)
-      # Set the positions of the nodes in the graph
-      nx.set_node_attributes(G, pos, 'pos')
   return G
 
 
+def limitGraphComplexity(graph, max_nodes, max_edges):
+  if graph.number_of_nodes() > max_nodes:
+    # Remove nodes exceeding the maximum limit
+    nodes_to_remove = list(graph.nodes())[max_nodes:]
+    graph.remove_nodes_from(nodes_to_remove)
+
+  if graph.number_of_edges() > max_edges:
+    # Remove edges exceeding the maximum limit
+    edges_to_remove = list(graph.edges())[max_edges:]
+    graph.remove_edges_from(edges_to_remove)
+
+  return graph
+
+
+app.run(debug=True)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
